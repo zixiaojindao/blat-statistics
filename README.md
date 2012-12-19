9713be91e1215bb52e69fa7f944384a5d1bb337d
Sun Zhao <zixiaojindao@gmail.com>
2012-12-19 16:43:31 +0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 1. This program is used to parse psl file which is output of blat for some useful information.
 
-2. Usage: ./blat-statistics path2output.psl path2ref.fa
+2. Usage: ./blat-statistics path2output.psl path2ref.fa(database) path2query.fa(query).  
 
-3. blat-statistics first add two additional coloumns named pid(percent idenity) and score. Methods for calculating pid and score are described on [Blat FAQ](http://genome.ucsc.edu/FAQ/FAQblat.html#blat4).
+3. blat-statistics first adds two additional coloumns named pid(percent idenity) and score, and then print some statistics at the end of output file. Methods for calculating pid and score are described on [Blat FAQ](http://genome.ucsc.edu/FAQ/FAQblat.html#blat4).
 
 4. Find exes in public directory.
 
--- blat-statistics/run.cpp
+++ blat-statistics/run.cpp
@@ -5,22 +5,48 @@
 #include<sstream>
 #include<vector>
 #include "psl.h"
+#include<algorithm>
+#include<numeric>
 using namespace std;
 
 void PrintHelp()
 {
-	cout<<"./blat-statistics path2output.psl path2ref.fa"<<endl;
+	cout<<"./blat-statistics path2output.psl path2ref.fa path2query.fa"<<endl;
+}
+
+inline bool isACGT(char c)
+{
+	return c == 'A' || c == 'C' || c == 'G' || c == 'T'
+		|| c == 'a' || c == 'c' || c == 'g' || c == 't';
+}
+
+int NX0(vector<int>& lens, double px0)
+{
+	int nx0Acc = 0;
+	int nx0 = 0;
+	for(vector<int>::iterator itc = lens.begin(); itc != lens.end(); ++itc)
+	{ 
+		nx0Acc += *itc;
+		if(nx0Acc > px0)
+		{
+			nx0 = *itc;
+			break;
+		}
+	}	
+	return nx0;
 }
 
 int main(int argc, char** argv)
 {
-	if(argc < 3)
+	if(argc < 4)
 	{
 		PrintHelp();
 		exit(EXIT_FAILURE);
 	}
+	//open file
 	string blatFilePath = argv[1];
 	string refFilePath = argv[2];
+	string queryFilePath = argv[3];
 	ifstream fpBlat(blatFilePath);
 	if(!fpBlat)
 	{
@@ -33,23 +59,31 @@ int main(int argc, char** argv)
 		cout<<"error: open "<<refFilePath<<endl;
 		exit(EXIT_FAILURE);
 	}
+	ifstream fpQuery(queryFilePath);
+	if(!fpQuery)
+	{
+		cout<<"error: open "<<queryFilePath<<endl;
+		exit(EXIT_FAILURE);
+	}
+
 	string staOutPut = "stac.txt";
 	ofstream fpsta(staOutPut);
-
+	//read ref length
 	string line;
 	long long refLen = 0; 
 	while(getline(fpRef, line))
 		if(line.length() >0 && line[0]!='>')
 			refLen += line.length();
-	//cout<<refLen<<endl;
 	fpRef.close();
+
+	//read psl and calculate pid and score
 	string nonUseLine;
 	int header = 5;
 	vector<bool> ref(refLen, 0);
 	string oldQName = "";
 	int percent95 = 0;
 	int percent50 = 0;
-	int totalQuery = 0;
+	int totalBlatQuery = 0;
 	for(int i = 0; i < header; ++i)
 	{
 		getline(fpBlat, nonUseLine);
@@ -58,6 +92,7 @@ int main(int argc, char** argv)
 			fpsta<<"\tpid\tsocre";
 		fpsta<<endl;
 	}
+	vector<int> contigsLen;
 	double bestPid = 0;
 	vector<int> blockSizes;
 	vector<int> tStarts;
@@ -73,7 +108,7 @@ int main(int argc, char** argv)
 		if(oldQName != pslNode.qName)
 		{
 			oldQName = pslNode.qName;
-			totalQuery += 1;
+			totalBlatQuery += 1;
 			if(bestPid > 0.95)
 				percent95 += 1;
 			if(bestPid > 0.5)
@@ -97,6 +132,7 @@ int main(int argc, char** argv)
 				bestPid = pslNode.percentIdentity;
 				blockSizes = pslNode.blockSizes;
 				tStarts = pslNode.tStarts;
+				strand = pslNode.strand;
 			}
 		}
 	}
@@ -112,12 +148,41 @@ int main(int argc, char** argv)
 	for(vector<bool>::iterator it = ref.begin(); it != ref.end(); ++it)
 		if(*it)
 			sum += 1;
+
+	int tl = -1;
+	int totalQuery = 0;
+	while(getline(fpQuery, line))
+	{
+		if(line.length() > 0 && line[0] == '>')
+		{
+			if(tl != -1)
+				contigsLen.push_back(tl);
+			tl = 0;
+			totalQuery += 1;
+		}
+		else
+		{
+			tl += count_if(line.begin(), line.end(), isACGT);
+		}
+	}
+	if(tl != -1)
+		contigsLen.push_back(tl);
+
+	sort(contigsLen.rbegin(), contigsLen.rend());
+	int contigSum = accumulate(contigsLen.begin(), contigsLen.end(), 0);
+	int n90 = NX0(contigsLen, contigSum * 0.9); 
+	int n50 = NX0(contigsLen, contigSum * 0.5);
 	fpsta<<"------------------------------------------------"<<endl;
 	fpsta<<"ref length="<<refLen<<endl;
-	fpsta<<"query seq # ="<<totalQuery<<endl;
-	fpsta<<">95% map ratio ="<<percent95 / (double)totalQuery * 100<<"%"<<endl;
-	fpsta<<">50% map ration ="<<percent50 / (double)totalQuery * 100<<"%"<<endl;
+	fpsta<<"blat query # ="<<totalBlatQuery<<endl;
+	fpsta<<"total query # ="<<totalQuery<<endl;
+	fpsta<<">95% map ratio :"<<percent95<<" / "<<totalQuery<<"="<<percent95 / (double)totalQuery * 100<<"%"<<endl;
+	fpsta<<">50% map ratio :"<<percent50<<" / "<<totalQuery<<"="<<percent50 / (double)totalQuery * 100<<"%"<<endl;
 	fpsta<<"ref coverage ="<<sum / (double)refLen * 100<<"%"<<endl;
+	fpsta<<"max query ="<<(contigsLen.size() == 0? 0:contigsLen[0])<<endl;
+	fpsta<<"n50 ="<<n50<<endl;
+	fpsta<<"n90 ="<<n90<<endl;
 	fpBlat.close();
 	fpsta.close();
+	fpQuery.close();
 }
